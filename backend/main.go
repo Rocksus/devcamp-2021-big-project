79d24c16c2edbd5a82caa08c1096a46f3475f199
Ray Antonius <rayantonius@gmail.com>
2021-07-05 08:56:02 +0700

    update cache config

--- backend/main.go
+++ backend/main.go
@@ -19,9 +19,9 @@ import (
 
 func main() {
 	cacheConfig := cache.Config{
-		MaxActive:       20,
+		MaxActive:       200,
 		MaxIdle:         5,
-		IdleTimeout:     240 * time.Second,
+		IdleTimeout:     3 * time.Second,
 		MaxConnLifetime: 0,
 		Address:         "redis:6379",
 	}

dd73a4b060d18160e354cf9535ef666926613930
Muhamad Hilmy Haidar <hilmy_21@apps.ipb.ac.id>
2021-07-07 01:21:18 +0700

    finish GetProductBatch with search in handler

--- backend/server/handlers/product/handler.go
+++ backend/server/handlers/product/handler.go
@@ -2,7 +2,6 @@ package product
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -78,6 +77,8 @@ func (p *Handler) GetProduct(w http.ResponseWriter, r *http.Request) {
 }
 
 func (p *Handler) GetProductBatch(w http.ResponseWriter, r *http.Request) {
+	timeStart := time.Now()
+	
 	var limit int
 	var offset int
 
@@ -92,10 +93,15 @@ func (p *Handler) GetProductBatch(w http.ResponseWriter, r *http.Request) {
 	if err != nil || offset < 0 {
 		offset = 0
 	}
+	search := vars.Get("search")
 
-	// TODO: remove the placeholder println and implement this function
-	fmt.Println(limit, offset)
+	resp, err := p.product.GetProductBatch(limit, offset, search)
+	if err != nil {
+		server.RenderError(w, http.StatusBadRequest, err, timeStart)
+		return
+	}
 
+	server.RenderResponse(w, http.StatusOK, resp, timeStart)
 	return
 }
 
